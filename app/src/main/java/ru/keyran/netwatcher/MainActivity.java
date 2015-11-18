abb490e0a20f0a3b008584915db34e55c35025f9
Keyran <keyran@git.av>
2015-11-18 11:10:29 +0300

    Add showing connectivity status at startup

--- app/src/main/java/ru/keyran/netwatcher/MainActivity.java
+++ app/src/main/java/ru/keyran/netwatcher/MainActivity.java
@@ -1,7 +1,11 @@
 package ru.keyran.netwatcher;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.manager = (ConnectivityManager) this.getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo net = this.manager.getActiveNetworkInfo();
+        TextView connstat = (TextView) findViewById(R.id.connectionStatusText);
+        if (net == null){
+            connstat.setText(R.string.disconnected);
+        }
+        else connstat.setText(R.string.connected);
     }
+    private ConnectivityManager manager;
 }
