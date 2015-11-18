e15e2b25ea8cc58dd21324c90aa223fb44f81a4f
Keyran <keyran@git.av>
2015-11-18 16:28:27 +0300

    A browser button added

--- app/src/main/java/ru/keyran/netwatcher/MainActivity.java
+++ app/src/main/java/ru/keyran/netwatcher/MainActivity.java
@@ -6,8 +6,11 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -30,7 +33,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         changeConnectionStatus();
-        registerReceiver(receiver,filter);
+        registerReceiver(receiver, filter);
 
     }
 
@@ -41,13 +44,24 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void openBrowser(View v){
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse("http://google.com"));
+        startActivity(intent);
+    }
+
     private void changeConnectionStatus() {
         NetworkInfo net = this.manager.getActiveNetworkInfo();
         TextView connstat = (TextView) findViewById(R.id.connectionStatusText);
+        Button browser = (Button) findViewById(R.id.browser_button);
         if (net == null){
             connstat.setText(R.string.disconnected);
+            browser.setVisibility(View.INVISIBLE);
         }
-        else connstat.setText(R.string.connected);
+        else{
+            connstat.setText(R.string.connected);
+            browser.setVisibility(View.VISIBLE);
+        }
     }
 
     private ConnectivityManager manager;
